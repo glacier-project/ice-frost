671fb59aec0f2dbe7c71e3345c8e4186e391b413
pt199vr <pt99vr@gmail.com>
2025-08-01 17:16:59 +0200

    Conveyor Fix

--- src/Conveyor/source_folder/bay_algorithm.py
+++ src/Conveyor/source_folder/bay_algorithm.py
@@ -3,12 +3,21 @@ def Bay_Routing(position, destination, bay):
         destination = "A1"
     elif len(destination) == 1:
         destination = "A1"
+    n = position[1]
+    if "A1" == destination:
+        if n == 1:
+            return "CROSS", bay + "1"
+        elif n == 2:
+            return "CROSS", bay + "2"
+        elif n == 3:
+            return "OUT", bay + "3"    
+    
     if position[1] == "1" :
         if destination[0] != position[0]:
             return "CROSS", bay + "1"
         elif destination[1] in ["2", "3"]:
             return "OUT", bay + "P1"
-    
+            
     elif position[1] == "2" :
         if destination[0] != position[0]:
             return "CROSS", bay + "2"
