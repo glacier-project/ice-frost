378fa2edfaa111d3a2c192927a881368ec8770e2
pt199vr <pt99vr@gmail.com>
2025-08-22 17:38:50 +0200

    Pallets destination link between data model and model

--- src/Conveyor/source_folder/pallet.py
+++ src/Conveyor/source_folder/pallet.py
@@ -9,4 +9,8 @@ class Pallet:
         self.position:str = position
         self.action:str = None
         self.bay:bool = None
-        self.object:Material = None
+        self.object1:Material = None
+        self.object2:Material = None
+        self.object3:Material = None
+        self.reserved:bool = False
+        self.type:str = ""
