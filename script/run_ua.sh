e8329c604a62ad90df1bd85c0c2a25d7fae70cf8
Pietro <pt99vr@gmail.com>
2026-03-06 17:09:29 +0100

    fix(demo lego): scripts and config files

--- script/run_ua.sh
+++ script/run_ua.sh
@@ -3,7 +3,7 @@
 METRICS=("DISTANCE")
 INSTANCES=("instance_empty.json") # "instance_mini.json" "instance_small.json" "instance_medium.json" "instance_large.json")
 OUTPUT_DIR="results/opcua-$(date +%Y%m%d_%H%M%S)"
-cd ..
+cd "$(dirname "$0")/.."
 FROST_CONFIG="resources/frost_config_ua.yml"
 echo "Running simulation with FROST_CONFIG=${FROST_CONFIG} METRIC=${METRIC} and INSTANCE=${INSTANCE}"
 INSTANCE_PATH="resource/instance/${INSTANCE}"
